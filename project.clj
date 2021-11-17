7e3bbcbe80e137c5419984a040d433060a476c18
高橋 康治 <takahashikouji@Mac351-TAKAHASHI.local>
2021-11-17 16:32:57 +0900

    Use own customized xray-sdk

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
-(defproject com.github.hden/aws-xray-sdk-clj "0.1.0-SNAPSHOT"
+(defproject com.github.popoppo/aws-xray-sdk-clj "0.1.0-SNAPSHOT"
   :description "A light wrapper for aws-xray-sdk-java"
-  :url "https://github.com/hden/aws-xray-sdk-clj"
+  :url "https://github.com/popoppo/aws-xray-sdk-clj"
   :license {:name "Apache License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0"}
-  :java-source-paths ["src/java"]
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.checkerframework/checker "3.19.0"]
-                 [com.amazonaws/aws-xray-recorder-sdk-core "2.10.0"]]
+                 [com.github.popoppo/aws-xray-recorder-sdk-core "0.1.0-SNAPSHOT"]]
   :repl-options {:init-ns aws-xray-sdk-clj.core})
